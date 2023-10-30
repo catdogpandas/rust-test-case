200614bfb9b19233ef4f5ac47122864017e610b2
liqiang <liqiang.x@foxmail.com>
2023-10-30 22:42:23 +0800

    feat: 增加闭包逃逸越界访问检查

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+fn check_case() {
+    let mut idx = 0;
+    let mut v = vec![1, 2, 3, 4, 5];
+    while idx < 5 {
+        idx = idx + 1;
+    }
+    v[idx] = idx;
+}
+fn uncheck_case() {
+    let mut idx = 0;
+    let mut v = vec![1, 2, 3, 4, 5];
+    let mut f = || {
+        while idx < 5 {
+            idx = idx + 1;
+        }
+    };
+    f();
+    v[idx] = idx;
+}
 fn main() {
-    println!("Hello, world!");
+    check_case();
+    uncheck_case();
 }
