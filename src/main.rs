eb210d142ef952d6e8f8ceb3f27150d562d2b16c
liqiang <321259693@qq.com>
2023-11-09 16:21:46 +0800

    chore: 更正路径

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-pub mod stack_overflow;
+pub mod buffer_overflow;
 pub mod mirchecker_panic;
 pub mod dop;
 pub mod double_free;
@@ -6,11 +6,12 @@ pub mod out_of_bounds_access;
 pub mod integer_overflow;
 
 fn main() {
-    // stack_overflow::check_case();
+    buffer_overflow::check_case();
     // mirchecker_panic::mirchecker_panic();
     // dop::dop();
     // double_free::check_case();
+    // double_free::uncheck_case();
     // out_of_bounds_access::check_case();
     // out_of_bounds_access::uncheck_case();
-    integer_overflow::check_case();
+    // integer_overflow::check_case();
 }
