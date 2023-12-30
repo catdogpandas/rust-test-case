4f87be2b3b22900a13b24366a8f379a66788ffc4
cargo-static-checker <liqiang.x@foxmail.com>
2023-12-30 15:10:14 +0800

    feat: 增加函数指针、高阶函数的DF样例

--- src/double_free.rs
+++ src/double_free.rs
@@ -1,26 +1,48 @@
 //! Double Free 的测试样例
-//! 
 
 use std::vec::Vec;
 
 /// 可被检测到：Safedrop
 pub fn check_case() {
-    let mut a = vec![1,2];
+    let mut a = vec![1, 2];
     let ptr = a.as_mut_ptr();
-    unsafe{
+    unsafe {
         let mut _v = Vec::from_raw_parts(ptr, 2, 2);
     }
 }
 
 /// 不可被检测到：SafeDrop
-pub fn uncheck_case(){
-    let mut a = vec![1,2];
+pub fn closure_case() {
+    let mut a = vec![1, 2];
     let ptr = a.as_mut_ptr();
-    let f = ||{
-        unsafe{
-            let mut _v = Vec::from_raw_parts(ptr, 2, 2);
-        }
+    let f = || unsafe {
+        let mut _v = Vec::from_raw_parts(ptr, 2, 2);
     };
+    f();
+}
+
+/// 函数指针样例
+pub fn function_pointer_case() {
+    let mut a = vec![1, 2];
+    let ptr = a.as_mut_ptr();
+    let f = || unsafe {
+        let mut _v = Vec::from_raw_parts(ptr, 2, 2);
+    };
+    let f_pointer = Box::new(f);
+    f_pointer();
+}
+
+/// 高阶函数样例
+pub fn high_order_function_case() {
+    let mut a = vec![1, 2];
+    let ptr = a.as_mut_ptr();
+    let f = || unsafe {
+        let mut _v = Vec::from_raw_parts(ptr, 2, 2);
+    };
+    high_order_function_caller(f);
+}
+
+fn high_order_function_caller<F: FnOnce() -> ()>(f: F) {
     f();
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 pub mod buffer_overflow;
-pub mod mirchecker_panic;
+pub mod division_by_zero;
 pub mod dop;
 pub mod double_free;
-pub mod out_of_bounds_access;
 pub mod integer_overflow;
-pub mod division_by_zero;
+pub mod mirchecker_panic;
+pub mod out_of_bounds_access;
 pub mod use_after_free;
 
 fn main() {
@@ -12,7 +12,9 @@ fn main() {
     mirchecker_panic::mirchecker_panic();
     dop::dop();
     double_free::check_case();
-    double_free::uncheck_case();
+    double_free::closure_case();
+    double_free::function_pointer_case();
+    double_free::high_order_function_case();
     double_free::ffi_check_case();
     double_free::ffi_uncheck_case();
     out_of_bounds_access::check_case();
