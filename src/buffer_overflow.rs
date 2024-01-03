7a42d71f0d13821c0b84b0d889876fae5ffc53bc
liqiang <liqiang.x@foxmail.com>
2024-01-03 14:43:15 +0800

    feat: 缓冲区溢出样例

--- src/buffer_overflow.rs
+++ src/buffer_overflow.rs
@@ -1,5 +1,47 @@
 //! Buffer overflow 测试样例
 
+/// 栈溢出样例
+pub fn check_case() {
+    let x = 2;
+    let mut buf = Vec::new();
+    buf.push(0);
+    buf.push(1);
+    buf[x] = 2;
+}
+
+/// 栈溢出闭包样例
+pub fn closure_case() {
+    let mut buf = Vec::new();
+    buf.push(0);
+    let f = || 2;
+    let x = f();
+    buf[x] = 2;
+}
+
+/// 栈溢出函数指针样例
+pub fn function_pointer_case() {
+    let mut buf = Vec::new();
+    buf.push(0);
+    let f = || 2;
+    let f_pointer = Box::new(f);
+    let x = f_pointer();
+    buf[x] = 2;
+}
+
+/// 栈溢出高阶函数样例
+pub fn high_order_function_case() {
+    let mut buf = Vec::new();
+    buf.push(0);
+    let f = || 2;
+    let x = high_order_function_caller(f);
+    buf[x] = 2;
+}
+
+fn high_order_function_caller<F: FnOnce() -> usize>(f: F) -> usize {
+    f()
+}
+
+/// 展示栈溢出漏洞利用
 #[repr(C)]
 struct Hackvist {
     buffer: [u8; 16],
@@ -9,7 +51,7 @@ struct Hackvist {
 /// 可被检测到：
 /// 不可被检测到：SafeDrop、Mir-Checker
 #[allow(dead_code)]
-pub(crate) fn check_case() {
+pub(crate) fn show_case() {
     let input_string = String::from("testtesttesttestAAAAAAAA");
     let input_bytes: &[u8] = input_string.as_bytes();
     let mut hackvist = Hackvist {
--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,33 @@ pub mod use_after_free;
 
 fn main() {
     buffer_overflow::check_case();
-    mirchecker_panic::mirchecker_panic();
-    dop::dop();
+    buffer_overflow::closure_case();
+    buffer_overflow::function_pointer_case();
+    buffer_overflow::high_order_function_case();
+
     double_free::check_case();
     double_free::closure_case();
     double_free::function_pointer_case();
     double_free::high_order_function_case();
     double_free::ffi_check_case();
     double_free::ffi_uncheck_case();
-    out_of_bounds_access::check_case();
-    out_of_bounds_access::uncheck_case();
-    integer_overflow::check_case();
-    integer_overflow::uncheck_case();
-    division_by_zero::check_case();
-    division_by_zero::uncheck_case();
+
     use_after_free::check_case();
     use_after_free::uncheck_case();
     use_after_free::ffi_check_case();
     use_after_free::ffi_uncheck_case();
+
+    out_of_bounds_access::check_case();
+    out_of_bounds_access::uncheck_case();
+
+    integer_overflow::check_case();
+    integer_overflow::uncheck_case();
+
+    division_by_zero::check_case();
+    division_by_zero::uncheck_case();
+
+    // show case
+    buffer_overflow::show_case();
+    dop::dop();
+    mirchecker_panic::mirchecker_panic();
 }
